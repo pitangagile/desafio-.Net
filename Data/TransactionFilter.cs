968264caaf1c4faf5664440c4fc05e62b7538af6
Jurandir P. A. Santos <jurandir.santos@pitang.com>
2019-04-08 13:59:38 -0300

    Transação

---
+++ Data/TransactionFilter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Data
+{
+	public class TransactionFilter : IAsyncActionFilter
+	{
+		private readonly DbTransaction transaction;
+
+		public TransactionFilter(DbTransaction transaction)
+		{
+			this.transaction = transaction;
+		}
+
+		public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+		{
+			var connection = transaction.Connection;
+			if (connection.State != ConnectionState.Open)
+				throw new NotSupportedException("The provided connection was not open!");
+
+			var executedContext = await next.Invoke();
+			if (executedContext.Exception == null)
+			{
+				transaction.Commit();
+			}
+			else
+			{
+				transaction.Rollback();
+			}
+		}
+	}
+}
---
+++ DesafioDotNET/Tools/WebHostExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DesafioDotNET
+{
+	public static class WebHostExtensions
+	{
+		//public static IWebHost UpgradeRegisterDb(this IWebHost webHost)
+		//{
+		//	var connectionString = new ConfigurationBuilder()
+		//		  .SetBasePath(Directory.GetCurrentDirectory())
+		//		  .AddJsonFile("appsettings.json")
+		//		  .Build().GetConnectionString("RegisterDb");
+
+		//	var options = new DbContextOptionsBuilder<RegisterDbContext>()
+		//			.UseSqlServer(connectionString)
+		//			.Options;
+
+		//	var dbContext = new RegisterDbContext(options);
+		//	dbContext.Database.Migrate();
+
+		//	return webHost;
+		//}
+	}
+}
